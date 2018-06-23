9459a67b3c195b9efbd1b6af1c44829b2f564577
chamao <44nocycle@gmail.com>
2018-06-24 08:23:09 +0900

    headerリンクをvue実装

--- app/javascript/packs/todo.js
+++ app/javascript/packs/todo.js
@@ -1,8 +1,10 @@
 import Vue from 'vue/dist/vue.esm.js'
+import Router from './router/router'
 import App from '../app.vue'
 import Header from './components/header.vue'
 
 var app = new Vue({
+  router: Router,
   el: '#app',
   components: {
     'navbar': Header,
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'top/index'
   resources :todos
 
+  # api
   get 'api/todos/index' => 'api/todos#index'
   get 'api/todos/show/:todo_id' => 'api/todos#show'
   post 'api/todos/update/:todo_id' => 'api/todos#update'
+
+  # header
+  get '/about',   to: 'home#index'
+  get '/contact', to: 'home#index'
 end
